90887d52d39dee44f9b2c6febc699131fceb8438
ngocminh2606 <23020851@vnu.edu.vn>
2025-10-07 10:22:44 +0700

    Update MainProgram.java

--- baitap/src/MainProgram.java
+++ baitap/src/MainProgram.java
@@ -1,28 +1,24 @@
-// MainProgram.java
 import java.util.ArrayList;
 
 public class MainProgram {
     public static void main(String[] args) {
         ArrayList<Shape> shapes = new ArrayList<>();
 
-        // Khởi tạo vài hình
         shapes.add(new Rectangle(new Point(1, 2), 4, 5));
         shapes.add(new Square(new Point(3, 3), 6));
         shapes.add(new Circle(new Point(0, 0), 10));
 
-        // In số lượng hình đã tạo
         System.out.println("Số lượng hình đã khởi tạo: " + Shape.getCount());
 
-        // In chi tiết các hình
         System.out.println("Danh sách chi tiết các hình:");
         for (Shape s : shapes) {
             System.out.println(s.getDetail());
         }
 
-        // In danh sách tâm điểm
         System.out.println("Danh sách tâm điểm:");
         for (Shape s : shapes) {
             System.out.println(s.getCenterPoint());
         }
     }
 }
+
